c4c847f8cf222b75d427ae3874072eadcbf80211
vinicius <vigusmao@gmail.com>
2018-10-29 11:09:46 -0200

    3nplus1.py com memo

--- 3nplus1.py
+++ 3nplus1.py
@@ -16,25 +16,19 @@ def g(n):
     if valor_do_memo is not None:
         return valor_do_memo
     
-    return 1 + g(f(n))
+    result = 1 + g(f(n))
+    memo[n] = result
+    return result
 
 while True:
+    memo.clear()
     lmin, lmax = eval(input("lmin, lmax = "))
     start = time()
     
     melhor_n = lmin
     maior_seq = g(lmin)
     for x in range(lmin + 1, lmax + 1):
-        # está no memo?
-        valor_do_memo = memo.get(x)
-        
-        # se não estiver, calculo e memoizo
-        if valor_do_memo is None:
-            tamanho = g(x)
-            memo[x] = tamanho
-        else: 
-            tamanho = valor_do_memo 
-
+        tamanho = g(x)
         if tamanho > maior_seq:
             melhor_n = x
             maior_seq = tamanho
